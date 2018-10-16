d79e886c98f5e144ce58f526b57980c456392a97
pooja <poojaputtu24@gmail.com>
2018-10-16 22:52:16 +0530

    year picker

---
+++ js/script.js
@@ -0,0 +1,69 @@
+function refreshLog() {
+    $("#logs").scrollTop($("#logs")[0].scrollHeight);
+}
+
+$(function() {
+    var currentYear = new Date().getFullYear();
+    var dataSource = $('#calendar').data('calendar').getDataSource();
+    $('#calendar').calendar({ 
+    mouseOnDay: function(e) {
+            if(e.events.length > 0) {
+                var content = '';
+                
+                for(var i in e.events) {
+                    content += '<div class="event-tooltip-content">'
+                                    + '<div class="event-name" style="color:' + e.events[i].color + '">' + e.events[i].name + '</div>'
+                                    + '<div class="event-location">' + e.events[i].location + '</div>'
+                                + '</div>';
+                }
+            
+                $(e.element).popover({ 
+                    trigger: 'manual',
+                    container: 'body',
+                    html:true,
+                    content: content
+                });
+                
+                $(e.element).popover('show');
+            }
+        },
+        mouseOutDay: function(e) {
+            if(e.events.length > 0) {
+                $(e.element).popover('hide');
+            }
+        },
+        dayContextMenu: function(e) {
+            $(e.element).popover('hide');
+        },
+        dataSource: [
+            {
+                id: 0,
+                name: 'Google I/O',
+                location: 'San Francisco, CA',
+                startDate: new Date(currentYear, 4, 28),
+                endDate: new Date(currentYear, 4, 28)
+            },
+            {
+                id: 1,
+                name: 'Microsoft Convergence',
+                location: 'New Orleans, LA',
+                startDate: new Date(currentYear, 2, 16),
+                endDate: new Date(currentYear, 2, 16)
+            },
+            {
+                id: 2,
+                name: 'Microsoft Build Developer Conference',
+                location: 'San Francisco, CA',
+                startDate: new Date(currentYear, 3, 1),
+                endDate: new Date(currentYear, 3, 1)
+            },
+            {
+                id: 3,
+                name: 'Apple Special Event',
+                location: 'San Francisco, CA',
+                startDate: new Date(currentYear, 8, 1),
+                endDate: new Date(currentYear, 8, 1)
+            }
+        ]
+    });
+});
